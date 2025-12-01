5be1934c6e84f61945dfa511bb09b9805f7b1cb8
javathunderman <javathunderman@outlook.com>
2025-12-01 13:26:34 -0600

    Detect array subscripts

--- custom-plugin/plugin.cpp
+++ custom-plugin/plugin.cpp
@@ -22,8 +22,11 @@ class AssertionVisitor : public RecursiveASTVisitor<AssertionVisitor> {
  public:
   explicit AssertionVisitor(ASTContext *Context) : context(Context) {}
   std::unordered_map<std::string, clang::Expr*> lexical_map;
+  std::unordered_map<std::string, clang::FunctionDecl *> kernel_map;
+  std::unordered_map<std::string, clang::VarDecl*> kernel_vars;
+  FunctionDecl *curr_func;
   bool VisitStmt(Stmt *stmt) {
-        #ifdef DEBUG_PLUGIN
+        #ifdef DEBUG_STMT
         stmt->printPretty(llvm::outs(),
                           nullptr,
                           context->getPrintingPolicy());
@@ -32,17 +35,58 @@ class AssertionVisitor : public RecursiveASTVisitor<AssertionVisitor> {
     }
     bool VisitVarDecl(VarDecl *VD) {
         // Check if the variable is defined (not just declared)
-        if (context->getSourceManager().getFileID(VD->getLocation()) == context->getSourceManager().getMainFileID() && VD->hasDefinition() && VD->getType().getAsString() == "dim3") {
-            llvm::outs() << "Variable defined: " << VD->getNameAsString() << "\n";
-            llvm::outs() << "Definition located at: "
-                        << VD->getLocation().printToString(context->getSourceManager())
-                        << "\n";
-            lexical_map[VD->getNameAsString()] = VD->getInit();
-        } else if (context->getSourceManager)
+        if (context->getSourceManager().getFileID(VD->getLocation()) == context->getSourceManager().getMainFileID() && VD->hasDefinition()) {
+            if (VD->getType().getAsString() == "dim3") {
+                #ifdef DEBUG_VAR_LOC
+                llvm::outs() << "Variable defined: " << VD->getNameAsString() << "\n";
+                llvm::outs() << "Definition located at: "
+                            << VD->getLocation().printToString(context->getSourceManager())
+                            << "\n";
+                #endif
+                lexical_map[VD->getNameAsString()] = VD->getInit();
+            } else if (VD->getType()->isIntegerType()) {
+                DeclContext *parentContext = VD->getDeclContext();
+                if (clang::FunctionDecl *funcDecl = llvm::dyn_cast<clang::FunctionDecl>(parentContext)) {
+                    if (kernel_map[funcDecl->getNameAsString()]) {
+                        llvm::outs() << "Belongs to a kernel function " << funcDecl->getNameAsString() << " " << VD->getNameAsString() << "\n";
+                        kernel_vars[VD->getNameAsString()] = VD;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool VisitArraySubscriptExpr(clang::ArraySubscriptExpr *arrSubExpr) {
+        Expr *indexExpr = arrSubExpr->getIdx();
+        Expr *baseExpr = arrSubExpr->getBase();
+        if (kernel_map[curr_func->getNameInfo().getName().getAsString()]) {
+            if (clang::ImplicitCastExpr *baseExprI = llvm::dyn_cast<clang::ImplicitCastExpr>(baseExpr)) {
+                baseExprI->getSubExprAsWritten()->printPretty(llvm::outs(),
+                                nullptr,
+                                context->getPrintingPolicy());
+                // llvm::outs() << "\n" << baseExprI->getSubExprAsWritten()->getStmtClassName() << "\nAttempt to print the parent, then indexExpr\n";
+                if (clang::ImplicitCastExpr *indexExprI = llvm::dyn_cast<clang::ImplicitCastExpr>(indexExpr)) {
+                    #ifdef DEBUG_ARR_SUBSCRIPT
+                    llvm::outs()<< "Implicit cast expr for array subscript\n";
+                    #endif
+                    indexExprI->getSubExprAsWritten()->printPretty(llvm::outs(),
+                                nullptr,
+                                context->getPrintingPolicy());
+                } else if (clang::BinaryOperator *indexExprB = llvm::dyn_cast<clang::BinaryOperator>(indexExpr)) {
+                    #ifdef DEBUG_ARR_SUBSCRIPT
+                    llvm::outs()<< "BinOp for array subscript\n";
+                    #endif
+                    indexExprB->printPretty(llvm::outs(),
+                                nullptr,
+                                context->getPrintingPolicy());
+                }
+                llvm::outs() << "\n";
+            }
+        }
         return true;
     }
     bool VisitCallExpr(CallExpr *CE) {
-        // Check if the expression is a kernel launch pattern
         if (CE->getNumArgs() >= 2) {
             SourceManager &SM = context->getSourceManager();
             Expr *FirstArg = CE->getArg(0);
@@ -50,8 +94,8 @@ class AssertionVisitor : public RecursiveASTVisitor<AssertionVisitor> {
             if (FirstArg->getType().getAsString() == "dim3" && 
                 SecondArg->getType().getAsString() == "dim3") {
                 
-                llvm::outs() << "Found CUDA kernel launch: "
-                             << CE->getDirectCallee()->getNameInfo().getAsString() << "\n";
+                // llvm::outs() << "Found CUDA kernel launch: "
+                //              << CE->getDirectCallee()->getNameInfo().getAsString() << "\n";
                 
                 SourceRange range = FirstArg->getSourceRange();
                 if (range.isValid()) {
@@ -82,12 +126,12 @@ class AssertionVisitor : public RecursiveASTVisitor<AssertionVisitor> {
         return true;
     }
     bool VisitFunctionDecl(FunctionDecl *f) {
+        curr_func = f;
         if (f->hasAttr<CUDAGlobalAttr>()) {
-            llvm::outs() << "Found kernel function: " << f->getNameInfo().getName().getAsString() << "\n";
+            kernel_map[f->getNameInfo().getName().getAsString()] = f;
         }
         return true;
     }
-
  private:
   ASTContext *context;
 };
